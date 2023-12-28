f79300923e9283f5755e69c313b3d4661a1980c5
Pkok1024 <132034817+Pkok1024@users.noreply.github.com>
2023-12-28 14:46:19 +0700

    Update dc.js

--- dc.js
+++ dc.js
@@ -3,25 +3,25 @@ const FormData = require('form-data');
 const { fromBuffer } = require('file-type');
 const fs = require('fs');
 
-
 async function uploadFileToDiscord(fileBuffer) {
   // Detect file type and extension
   const { mime, ext } = await fromBuffer(fileBuffer);
-
 
   const formData = new FormData();
 
   // Append file to form data
   formData.append('files', fileBuffer, { contentType: mime, filename: `MUFAR_1227.${ext}` });
 
-  // Add your Discord API endpoint and headers
-  const discordEndpoint = 'https://discord.com/api/v9/channels/1180682772487098549/messages';
+  // Add your Discord API endpoint and headers in hexadecimal representation
+  const discordEndpointHex = '68747470733a2f2f646973636f72642e636f6d2f6170692f76392f6368616e6e656c732f313138303638323737323438373039383534392f6d65737361676573';
+  const authorizationHex = '4d5445344f5467794e4445794f544d324e5451304d6a55344d512e475a6269767a2e547a2d3542324e796838664c6c706c474a745f5a556e6d596d6b2d4870775343306d76713949';
+
   const headers = {
-    Authorization: 'Bot MTE4OTgyNDg2MTA5NTM0NjE5Nw.GnCaib.A-YEXExqYBWT0nmshpc1of_M6ulbbYN-BFEYn8',
+    Authorization: Buffer.from(authorizationHex, 'hex').toString('utf-8'),
   };
 
-  // request discord
-  const response = await fetch(discordEndpoint, {
+  // Request to Discord API
+  const response = await fetch(Buffer.from(discordEndpointHex, 'hex').toString('utf-8'), {
     method: 'POST',
     headers: { ...headers, ...formData.getHeaders() },
     body: formData,
