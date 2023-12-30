690f9462b30f05223d3073ab04fbcbb243578671
Pkok1024 <132034817+Pkok1024@users.noreply.github.com>
2023-12-30 07:46:44 +0000

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // index.js
 const {app} = require('./server'); // Assuming your server file is named 'server.js'
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
